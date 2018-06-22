4bac501b863de29ffc5a11813770080d0229c422
sabhyatachadha <sabhyatachadha@users.noreply.github.com>
2018-06-22 13:24:13 -0700

    created sum thing
    its aight or whatvr

---
+++ project.py
@@ -0,0 +1,8 @@
+num1 = 0
+num2 = 0
+
+num1 += 1
+num2 = num2 +1
+
+print( num1 )
+print ( num2 )
---
+++ woohoo.py
@@ -0,0 +1,13 @@
+num1=0
+num2=0
+
+sum= num1+num2
+
+num1= (input (" what is your fave type of dog "))
+num2= (input ("what is another fave type of dog: "))
+
+sum= num1+num2
+
+
+
+print("here is a new mix of dog just for u: %d " %sum)
